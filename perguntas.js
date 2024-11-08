ee4154729941c5df1d34241810e23e6b82c52bb2
31-MSG <00001116687045sp@al.educacao.sp.gov.br>
2024-11-08 17:26:31 -0300

    Cartões

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é programação?',
+    'É criar instruções para o computador em sua própia linguagem.'
+)
+
+criaCartao(
+    'Geografia',
+    'Onde se localiza Machu Picchu?',
+    'Peru'
+)
+
+criaCartao(
+    'Geografia',
+    'Quantos continentes existem?',
+    '6'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz como vai você em Inglês?',
+    'Se diz how are you'
+)
